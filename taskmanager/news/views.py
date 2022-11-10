b51245811f61e04426439be80b613ac0d5d729da
Vkalypal <Ljotych@gmail.com>
2022-11-10 22:06:35 +0600

    Финальная версия с уроков

--- taskmanager/news/views.py
+++ taskmanager/news/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/news-delete.html'
+    success_url = '/news/'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
 
 
 def create(request):
